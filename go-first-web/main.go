c5d1dde81c46304956ee805f2179b0ee25bea131
Youngchan <michael_cho77@naver.com>
2021-05-29 00:50:23 +0900

    ADD - Map, Slices

--- go-first-web/main.go
+++ go-first-web/main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"sort"
 )
 
-type myStruct struct {
+type User struct {
 	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
+	LastName  string
 }
 
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	// maps
+	myMap := make(map[string]User)
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
+	me := User{
+		FirstName: "Trevor",
+		LastName:  "Sawler",
 	}
-	fmt.Println(myVar.printFirstName())
-	fmt.Println(myVar2.printFirstName())
 
+	myMap["me"] = me
+
+	log.Println(myMap["me"])
+	log.Println(myMap["me"].FirstName)
+
+	// slices
+	var mySlice []int
+
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
+
+	sort.Ints(mySlice)
+
+	log.Println(mySlice)
+
+	// shorthand for slices
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
+
+	// print first two elements of slice, starting at first position
+	log.Println(numbers[0:2])
+
+	// create a slice of strings
+	names := []string{"one", "seven", "fish", "cat"}
+	log.Println(names)
 }
