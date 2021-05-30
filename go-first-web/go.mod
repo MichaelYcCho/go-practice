4e311dd10881c71e56e5b8411a41f748fe58a89a
Youngchan <michael_cho77@naver.com>
2021-05-30 23:58:44 +0900

    ADD - Channels

---
+++ go-first-web/go.mod
@@ -0,0 +1,3 @@
+module github.com/michael_cho77/go-practice/go-first-web
+
+go 1.16
--- go-first-web/main.go
+++ go-first-web/main.go
@@ -1,57 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+
+	"github.com/michael_cho77/go-practice/go-first-web/helpers"
+)
+
+const numPool = 1000
+
+func CalculateValue(intChan chan int) {
+	RandomNumber := helpers.RandomNumber(numPool)
+	intChan <- RandomNumber
+
+}
 
 func main() {
-	// the if statement
-	var isTrue bool
+	intChan := make(chan int)
+	defer close(intChan)
 
-	isTrue = false
+	go CalculateValue(intChan)
 
-	if isTrue == true {
-		log.Println("isTrue is", isTrue)
-	}
-	log.Println("isTrue is", isTrue)
-
-	cat := "cat2"
-
-	if cat == "cat" {
-		log.Println("Cat is cat")
-	} else {
-		log.Println("Cat is not cat")
-	}
-
-	myNum := 100
-	isTrue = false
-
-	if myNum > 99 && !isTrue {
-		log.Println("myNum is greater than 99 and isTrue is set to true")
-	} else if myNum < 100 && isTrue {
-		// do something
-	} else if myNum == 101 || isTrue {
-		// do something
-	} else if myNum > 1000 && isTrue {
-		// do something
-	}
-
-	// the switch statement
-
-	myVar := "cat"
-
-	switch myVar {
-	case "cat":
-		log.Println("myVar is set to cat")
-
-	case "dog":
-		log.Println("myVar is set to dog")
-
-	case "horse":
-		log.Println("myVar is set to horse")
-
-	case "fish":
-		log.Println("myVar is set to fish")
-
-	default:
-		log.Println("myVar is something else")
-	}
+	num := <-intChan
+	fmt.Println(num)
 }
