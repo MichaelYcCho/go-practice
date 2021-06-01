0b52b204613b33f039c48d37e804d699297d867a
Youngchan <michael_cho77@naver.com>
2021-06-02 00:21:03 +0900

    ADD - Functions and Handlers

--- go-first-web/main.go
+++ go-first-web/main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is Home")
+}
+
+// About is about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d"), sum)
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number fo bytes written: %d", n))
-	})
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
