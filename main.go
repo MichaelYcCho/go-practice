5aa51d39e6f824c04dd8bdb6811997bf9c155523
Youngchan <michael_cho77@naver.com>
2021-07-12 22:26:14 +0900

    feature/Adding A Block, Refactoring

--- main.go
+++ main.go
@@ -1,35 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
-
-	"github.com/michael_cho77/go-michael-coin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
+import "github.com/michael_cho77/go-michael-coin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
+
 }
