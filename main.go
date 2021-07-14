cd1c2c79e398d587e9d0c59c8ea4b236878dc964
Youngchan <michael_cho77@naver.com>
2021-07-14 21:40:50 +0900

    feature/Json Decode, New ServeMux

--- main.go
+++ main.go
@@ -1,53 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/michael_cho77/go-michael-coin/explorer"
+	"github.com/michael_cho77/go-michael-coin/rest"
 )
 
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-func (u URLDescription) String() string {
-	return "Hello I'm URL"
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
-const port string = ":4000"
-
 func main() {
-	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
-
+	go explorer.Start(3000)
+	rest.Start(4000)
 }
