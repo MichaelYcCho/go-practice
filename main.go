e63307d932ec009609d9afe7d56b617da21fd246
Youngchan <michael_cho77@naver.com>
2021-07-15 19:31:06 +0900

    feature/GorillaMux
    go get -u github.com/gorilla/mux

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/michael_cho77/go-michael-coin/explorer"
 	"github.com/michael_cho77/go-michael-coin/rest"
 )
 
 func main() {
-	go explorer.Start(3000)
 	rest.Start(4000)
 }
