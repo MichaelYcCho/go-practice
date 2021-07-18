78e2bc9265324305ebe336fd3e964920d34738f5
Youngchan <michael_cho77@naver.com>
2021-07-18 19:09:41 +0900

    feature/Saving A Block

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/boltdb/bolt"
 	"github.com/michael_cho77/go-michael-coin/utils"
 )
@@ -17,14 +19,33 @@ func DB() *bolt.DB {
 	if db == nil {
 		dbPointer, err := bolt.Open(dbName, 0600, nil)
 		db = dbPointer
-		utils.HandlerErr(err)
+		utils.HandleErr(err)
 		err = db.Update(func(t *bolt.Tx) error {
 			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
-			utils.HandlerErr(err)
+			utils.HandleErr(err)
 			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
 			return err
 		})
-		utils.HandlerErr(err)
+		utils.HandleErr(err)
 	}
 	return db
+}
+
+func SaveBlock(hash string, data []byte) {
+	fmt.Printf("Saving Block %s\nData: %b\n", hash, data)
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		err := bucket.Put([]byte(hash), data)
+		return err
+	})
+	utils.HandleErr(err)
+}
+
+func SaveBlockchain(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Put([]byte("checkpoint"), data)
+		return err
+	})
+	utils.HandleErr(err)
 }
