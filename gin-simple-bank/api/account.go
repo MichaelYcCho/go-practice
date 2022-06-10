54c337611c95bd65b0068ca08c2add63b2a34bca
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-06-11 00:16:47 +0900

    add: Main.go Setting

--- gin-simple-bank/api/account.go
+++ gin-simple-bank/api/account.go
@@ -8,15 +8,15 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner"`
-	Currency string `json:"currency" binding:"required,oneof=UED EUR"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 
 	// err가 nil이 아니면 유효하지않은 data를 제공했음을 의미한다.
-	if err := ctx.ShouldBind(&req); err != nil {
+	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
