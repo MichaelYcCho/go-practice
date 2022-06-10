021f47c32923393856c88f0942d19d1765cdc1ea
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-06-10 23:55:56 +0900

    add: Gin Setting

---
+++ gin-simple-bank/api/account.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/MichaelYcCho/go-practice/gin-simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner"`
+	Currency string `json:"currency" binding:"required,oneof=UED EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+
+	// err가 nil이 아니면 유효하지않은 data를 제공했음을 의미한다.
+	if err := ctx.ShouldBind(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
---
+++ gin-simple-bank/api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "github.com/MichaelYcCho/go-practice/gin-simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves Http requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new Http server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts")
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+
+}
