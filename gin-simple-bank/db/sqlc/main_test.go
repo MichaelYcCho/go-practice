d42dfa6ec0a6d57d3bdb020df32117ca922ef59f
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-06-14 22:33:36 +0900

    add: Using .env with Viper

--- gin-simple-bank/db/sqlc/main_test.go
+++ gin-simple-bank/db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/MichaelYcCho/go-practice/gin-simple-bank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:postgres@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
---
+++ go-react-gin-music/main.py
@@ -0,0 +1,4 @@
+
+user = None
+
+user is None and print('와 이게되나')
