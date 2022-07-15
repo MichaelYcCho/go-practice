7251925fca5f5ec161cfecbb415ad5b17bb18b54
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-15 23:18:19 +0900

    feature: Member Create

---
+++ go-example/member-crud/member/application.go
@@ -0,0 +1,49 @@
+package member
+
+import (
+	"log"
+	"strconv"
+)
+
+type Application struct {
+	repository Repository
+}
+
+func NewApplication(repository Repository) *Application {
+	return &Application{repository: repository}
+}
+
+func (app *Application) Create(request CreateRequest) (CreateResponse, error) {
+	membershipBuilder := NewMembershipGenerator()
+	member_repository := app.repository
+	member_count := len(member_repository.data)
+
+	id := strconv.Itoa(member_count + 1)
+	log.Println(id)
+
+	membershipBuilder.
+		SetID(id).
+		SetUserName(request.UserName).
+		SetMembershipType(request.MembershipType)
+
+	membership, err := membershipBuilder.GetMembership()
+	if err != nil {
+		return CreateResponse{}, err
+	}
+
+	_, err = app.repository.
+		AddRepository(*membership)
+	if err != nil {
+		return CreateResponse{}, err
+	}
+
+	return CreateResponse{membership.ID, membership.MembershipType}, nil
+}
+
+func (app *Application) Update(request UpdateRequest) (UpdateResponse, error) {
+	return UpdateResponse{}, nil
+}
+
+func (app *Application) Delete(id string) error {
+	return nil
+}
