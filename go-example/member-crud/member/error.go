a7c256691d05b7ca3363762d90aeabf160f13525
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-21 21:03:02 +0900

    refactor: Error코드 수정

--- go-example/member-crud/member/error.go
+++ go-example/member-crud/member/error.go
@@ -6,7 +6,7 @@ var (
 	ErrUserAlreadyExists        = errors.New("user already exists")
 	ErrUserNameIsRequired       = errors.New("user name is required")
 	ErrMembershipTypeIsRequired = errors.New("membership type is required")
-	ErrInvalidMembershipType    = errors.New("invalid membership type")
+	ErrInvalidMembershipType    = errors.New("choose membership type : naver, payco, toss")
 	ErrUserIDNotFound           = errors.New("user id not found")
 	ErrUserIDIsRequired         = errors.New("user id is required")
 )
