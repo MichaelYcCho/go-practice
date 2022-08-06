1ea5629aed585809a363935d2b5729d90754a436
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-08-06 20:19:19 +0900

    feat: Pointer Example2

--- pratice/grammar/pointer.go
+++ pratice/grammar/pointer.go
@@ -12,3 +12,25 @@ func Pointer_01() {
 	z := 5 + *pointerToX
 	fmt.Println(z) // 15
 }
+
+type Foo struct {
+	Field1 string
+	Field2 int
+}
+
+// 함수로 구조체 전달을 포인터로 하여 항목을 채우는것보다
+
+func Wrongway(f *Foo) error {
+	f.Field1 = "val"
+	f.Field2 = 20
+	return nil
+}
+
+// 함수 내에서 구조체를 초기화 하고 반환하는 것이 좋다
+func Rightway(Foo) (Foo, error) {
+	f := Foo{
+		Field1: "val",
+		Field2: 20,
+	}
+	return f, nil
+}
