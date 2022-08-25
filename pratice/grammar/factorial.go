6498a831ec02f32581b5a94ebc18bf6345beeb3a
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-08-25 22:45:00 +0900

    feat: 재귀함수없이 loop를 통한 팩토리얼

--- pratice/grammar/factorial.go
+++ pratice/grammar/factorial.go
@@ -9,7 +9,18 @@ func factorial(n int) int {
 	return n * factorial(n-1)
 }
 
+func loopFact(n int) int {
+	total := 1
+	for ; n > 0; n-- {
+		total *= n
+	}
+	return total
+}
+
 func Factorial() {
 	n := factorial(4)
 	fmt.Println(n)
+
+	n2 := loopFact(4)
+	fmt.Println(n2)
 }
