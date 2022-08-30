7f4f1818616d36fb41a2925773b2e897f7454aaf
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-08-31 07:39:39 +0900

    docs: 주석

--- pratice/grammar/sort_sturct.go
+++ pratice/grammar/sort_sturct.go
@@ -5,6 +5,8 @@ import (
 	"sort"
 )
 
+// https://pkg.go.dev/sort
+
 type SortPerson struct {
 	First string
 	Age   int
@@ -14,9 +16,11 @@ func (p SortPerson) String() string {
 	return fmt.Sprintf("%s: %d", p.First, p.Age)
 }
 
+// byAge와 people 모두 []SortPerson 타입이므로 변환을 쓸 수 있다.
 // type byAge[]sortPerson 으로 시작하는 코드 전체를 복사하여 핸들리이 가능하다
 type byAge []SortPerson
 
+//Sort 가 가지고있는 인터페이스의 내부메소드를 커스텀
 func (a byAge) Len() int           { return len(a) }
 func (a byAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a byAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
@@ -39,6 +43,7 @@ func SortStruct() {
 	fmt.Println(people)
 
 	fmt.Println("===============")
+	// people 타입을 Byage 타입으로 변환하여 sort.Sort() 함수에 전달한다.
 	sort.Sort(byAge(people))
 	fmt.Println(people)
 
