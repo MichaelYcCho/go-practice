2c50f127df29bc71c6fd3f6d25ddb367e75c00ad
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-04 23:09:32 +0900

    style: 주석

--- pratice/goroutine/simple_goroutine.go
+++ pratice/goroutine/simple_goroutine.go
@@ -9,8 +9,8 @@ func doSomething(x int) int {
 func RunSimpleGoroutine() {
 	ch := make(chan int)
 	go func() {
-		ch <- doSomething(5)
+		ch <- doSomething(5) // 반환값을 취해 채널(ch)에 붙인다음
 	}()
-	fmt.Println(<-ch)
+	fmt.Println(<-ch) // 그 채널에서 값을 끌어온다
 
 }
