c596fb535669e8534b0618bf6a60a6787f1efc81
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-05 00:23:02 +0900

    feat: Mutex Example

--- pratice/goroutine/race_condiction.go
+++ pratice/goroutine/race_condiction.go
@@ -16,13 +16,17 @@ func RaceFunction() {
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
+	var mu sync.Mutex
+
 	for i := 0; i < gs; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
 			//time.Sleep(time.Second)
 			runtime.Gosched() // Gosched는 다른 goroutine이 실행하게 하면스 그 프로세서를 양보한다.
 			v++
 			counter = v
+			mu.Unlock()
 			wg.Done()
 		}()
 	}
