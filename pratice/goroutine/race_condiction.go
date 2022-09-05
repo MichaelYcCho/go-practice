a2d8c9872a97f006447fa56a85a0f6e3921d0dcc
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-06 08:29:34 +0900

    feat: Atomic 예제

--- pratice/goroutine/race_condiction.go
+++ pratice/goroutine/race_condiction.go
@@ -4,31 +4,28 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 func RaceFunction() {
 	fmt.Println("CPU :", runtime.NumCPU())
 	fmt.Println("Goroutine :", runtime.NumGoroutine())
 
-	counter := 0
+	var counter int64
 
 	const gs = 100
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
-	var mu sync.Mutex
-
 	for i := 0; i < gs; i++ {
 		go func() {
-			mu.Lock()
-			v := counter
-			//time.Sleep(time.Second)
-			runtime.Gosched() // Gosched는 다른 goroutine이 실행하게 하면스 그 프로세서를 양보한다.
-			v++
-			counter = v
-			mu.Unlock()
+			atomic.AddInt64(&counter, 1)
+			runtime.Gosched()                                    // 보다 안전한 방식으로 counter에 write한다.
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter)) // 보다 안전한 방식으로 counter에 read한다.
+
 			wg.Done()
 		}()
+		fmt.Println("Goroutine :", runtime.NumGoroutine()) // Gosched는 다른 goroutine이 실행하게 하면스 그 프로세서를 양보한다.
 	}
 	wg.Wait()
 
