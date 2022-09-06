31a410dc7bf1b77843f41987da585f32479162fa
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-06 20:35:53 +0900

    style: Main 정리

---
+++ pratice/goroutine/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/MichaelYcCho/go-practice/pratice/goroutine"
+)
+
+func main() {
+
+	//Go Routine
+	//goroutine.RunTest01()
+	//goroutine.RunMethodSet()
+	//goroutine.RunSimpleGoroutine()
+	goroutine.RaceFunction()
+}
