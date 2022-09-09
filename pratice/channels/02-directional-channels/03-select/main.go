4be75454d797c796dd14707cb2b52a6f6284ff55
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-09 11:34:07 +0900

    feat: Comma, ok 문법

--- pratice/channels/02-directional-channels/03-select/main.go
+++ pratice/channels/02-directional-channels/03-select/main.go
@@ -1,46 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	eve := make(chan int)
+	even := make(chan int)
 	odd := make(chan int)
-	quit := make(chan int)
+	quit := make(chan bool)
 
 	//send
-	go send(eve, odd, quit)
+	go send(even, odd, quit)
 
 	//receive
-	receive(eve, odd, quit)
-	close(quit)
-
+	receive(even, odd, quit)
 	fmt.Println("about to exit")
 }
 
-func send(e, o, q chan<- int) {
+func send(even, odd chan<- int, quit chan<- bool) {
 	for i := 0; i < 100; i++ {
 		if i%2 == 0 {
-			e <- i
+			even <- i
 		} else {
-			o <- i
+			odd <- i
 		}
 	}
-	close(e)
-	close(o)
-	q <- 0
+	close(quit)
 
 }
 
-func receive(e, o, q <-chan int) {
+func receive(even, odd <-chan int, quit <-chan bool) {
 	for {
 		select {
-		case v := <-e:
+		case v := <-even:
 			fmt.Println("from the even channel:", v)
-		case v := <-o:
+		case v := <-odd:
 			fmt.Println("from the odd channel:", v)
-		case v := <-q:
-			fmt.Println("from the quit channel:", v)
-			return
+		case i, ok := <-quit:
+			if !ok {
+				fmt.Println("from comma ok", i, ok)
+				return
+			} else {
+				fmt.Println("from comma ok", i)
+			}
 		}
 	}
 }
