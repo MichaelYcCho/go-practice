d825c2cf7007265d448b6f8baed1c172615f56fc
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-13 18:38:20 +0900

    feat: Context Example2

--- pratice/channels/03-context/main.go
+++ pratice/channels/03-context/main.go
@@ -3,29 +3,42 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"time"
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 
-	fmt.Println("context: ", ctx)
-	fmt.Println("context err: ", ctx.Err())
-	fmt.Printf("context type: %T\t\n", ctx)
-	fmt.Println("=======================================")
+	fmt.Println("error check 1:", ctx.Err())
+	fmt.Println("num of goroutines 1:", runtime.NumGoroutine())
 
-	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		n := 0
+		for {
+			select {
+			// Done()은 해당 context를 위한 작업위 취소될때 닫힌 채널을 반환한다.
+			case <-ctx.Done():
+				return
+			// 위의 상황이 아니면 하기의 로직처럼 0.2초 슬립에 빠졌다가 n을 출력한다.
+			default:
+				n++
+				time.Sleep(time.Millisecond * 200)
+				fmt.Println("working", n)
+			}
+		}
+	}()
 
-	fmt.Println("context: ", ctx)
-	fmt.Println("context err: ", ctx.Err())
-	fmt.Printf("context type: %T\t\n", ctx)
-	fmt.Println("=======================================")
+	time.Sleep(time.Second * 2)
+	fmt.Println("error check 2:", ctx.Err())
+	fmt.Println("num of goroutines 2:", runtime.NumGoroutine())
 
+	fmt.Println("about to cancel context")
 	cancel()
+	fmt.Println("cancelled context")
 
-	fmt.Println("context: ", ctx)
-	fmt.Println("context err: ", ctx.Err())
-	fmt.Printf("context type: %T\t\n", ctx)
-	fmt.Println("cancel:\t\t", cancel)
-	fmt.Printf("cancel type: %T\t\n", cancel)
-	fmt.Println("=======================================")
+	time.Sleep(time.Second * 2)
+	fmt.Println("error check 3:", ctx.Err())
+	fmt.Println("num of goroutines 3:", runtime.NumGoroutine())
+
 }
