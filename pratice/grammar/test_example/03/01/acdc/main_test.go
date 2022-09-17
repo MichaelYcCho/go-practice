9a860627b3061cfa12994f9805889988c4516e75
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-09-17 15:17:54 +0900

    teat: Test Example Code
    Doc: 주석

---
+++ pratice/grammar/test_example/03/01/acdc/main_test.go
@@ -0,0 +1,11 @@
+package acdc
+
+func ExampleSum() {
+	Sum(2, 3)
+	Sum(2, 3, 4, 5, 6, 7, 8, 9)
+	// Output:
+	// 5
+	// 45
+}
+
+// godoc -http=:8080
---
+++ pratice/grammar/test_example/03/01/main.go
@@ -0,0 +1,9 @@
+
+package main
+
+import "github.com/MichaelYcCho/go-practice/pratice/grammar/test_example/03/01/acdc"
+
+func main() {
+	acdc.Sum(2, 3)
+	acdc.Sum(2, 3, 4, 5, 6, 7, 8, 9)
+}
