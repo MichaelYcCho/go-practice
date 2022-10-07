a0e29b5eb1e5b63f2a16dcff3ecf7753eab19655
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-10-07 23:11:12 +0900

    feat: Update 에러 해결

--- pratice/simple-book/controllers/book.go
+++ pratice/simple-book/controllers/book.go
@@ -1,12 +1,16 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/MichaelYcCho/go-practice/pratice/todo/models"
 	"github.com/gin-gonic/gin"
 )
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
 
 func FindBooks(c *gin.Context) {
 	var books []models.Book
@@ -36,28 +40,23 @@ func CreateBook(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"data": book})
 }
 
+// PATCH /books/:id
+// Update a book
 func UpdateBook(c *gin.Context) {
-	// Get model if exist
 	var book models.Book
 	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
 
-	// Validate input
 	var input UpdateBookInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println("인풋", input)
 
-	if er := models.DB.Model(&book).Updates(input); er.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": er.Error})
-		return
-	}
-
-	models.DB.Model(&book).Updates(input)
+	models.DB.Model(&book).Updates(models.Book{Title: input.Title, Author: input.Author})
+	// models.DB.Model(&book).Updates(map[string]interface{}{"Title": input.Title, "Author": input.Author})
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
--- pratice/simple-book/controllers/validator.go
+++ pratice/simple-book/controllers/validator.go
@@ -4,8 +4,3 @@ type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
 }
-
-type UpdateBookInput struct {
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
