56f78ed63478062fb5278d5e04a08291a16d9ce6
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-10-24 23:58:36 +0900

    refactor: .env 경로 재설정

--- pratice/simple-book/database/db.go
+++ pratice/simple-book/database/db.go
@@ -13,11 +13,10 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		panic("Error loading .env file")
 	}
-
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(127.0.0.1:3306)/gin_book_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
