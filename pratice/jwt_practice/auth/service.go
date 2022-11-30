4af0269b5ffd6e718772c49203bae5e7439199d2
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-11-30 22:50:40 +0900

    feat: ValidateToken

--- pratice/jwt_practice/auth/service.go
+++ pratice/jwt_practice/auth/service.go
@@ -1,9 +1,13 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 // 메서드를 만들기위해 빈 타입을 선언했다고 이해하는데 더 나은 구조는 없을까?
@@ -27,4 +31,22 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
