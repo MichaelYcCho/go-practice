bfb4771cb2ba79437509e73b13f63ccd6825637a
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-12-04 22:01:28 +0900

    feat: Campaign Service

---
+++ pratice/jwt_practice/campaign/service.go
@@ -0,0 +1,29 @@
+package campaign
+
+type Service interface {
+	FindCampaign(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	selector Selector
+}
+
+func NewService(selector Selector) *service {
+	return &service{selector}
+}
+
+func (s *service) FindCampaign(userID int) ([]Campaign, error) {
+	if userID != 0 {
+		campaigns, err := s.selector.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+		return campaigns, nil
+	}
+
+	campaigns, err := s.selector.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
