88cd3807a68585ed554c317fd42395bbc4d399c7
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-12-04 21:55:27 +0900

    feat: Campaign DB Migration, little Test

--- pratice/jwt_practice/main.go
+++ pratice/jwt_practice/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/MichaelYcCho/go-practice/pratice/jwt_practice/auth"
+	"github.com/MichaelYcCho/go-practice/pratice/jwt_practice/campaign"
 	"github.com/MichaelYcCho/go-practice/pratice/jwt_practice/database"
 	"github.com/MichaelYcCho/go-practice/pratice/jwt_practice/handler"
 	"github.com/MichaelYcCho/go-practice/pratice/jwt_practice/middleware"
@@ -16,10 +18,20 @@ func main() {
 	// Todo: 마이그레이션 폴더로 이동시키는것이 나을까?
 	modelList := []interface{}{
 		&users.User{},
+		&campaign.Campaign{},
+		&campaign.CampaignImage{},
 	}
 	db.AutoMigrate(modelList...)
 
 	userRepository := users.NewSelector(db)
+	campaignRepository := campaign.NewSelector(db)
+
+	campaigns, _ := campaignRepository.FindByUserID(1)
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		fmt.Println(campaign.CampaignImages[0].FileName)
+	}
+
 	userService := users.NewService(userRepository)
 	authService := auth.NewService()
 	userHandler := handler.NewUserHandler(userService, authService)
